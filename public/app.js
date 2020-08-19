b1bb42a1324f74ac72532869903270419d0d0f06
OSauceda <osfesa462@gmail.com>
2020-08-19 11:08:15 -0600

    WIP: setup for client app.

---
+++ public/app.js
@@ -0,0 +1,30 @@
+const socket = io();
+const client = feathers();
+
+client.configure(feathers.socketio(socket));
+
+client.configure(feathers.authentication({
+  storage: window.localStorage
+}));
+
+const login = async () => {
+  try {
+    return await client.reAuthenticate();
+  } catch(e) {
+    return await client.authenticate({
+      strategy: 'local',
+      email: 'hello@feathersjs.com',
+      password: '1234'
+    });
+  }
+};
+
+const main = async () => {
+  const auth = await login();
+
+  console.log('User is authenticated', auth);
+
+  await client.logout();
+};
+
+main();
